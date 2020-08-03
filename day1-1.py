b500fb899df5bdee46b678fa4910698e06370599
AAA152 <69131786+AAA152@users.noreply.github.com>
2020-08-03 11:15:03 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:21:55 2020
+
+@author: AE401
+"""
+print("I'm Eric")
+print("He's 'cool'")
+print("I'm Eric \nHe's 'cool'")
---
+++ day1-2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:43:50 2020
+
+@author: AE401
+"""
+
+name = input("What is your name?")
+print("Hi"+name)
