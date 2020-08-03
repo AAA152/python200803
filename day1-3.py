c248c1d5a6fe7dc288ea5592a8a418e0c822864a
AAA152 <69131786+AAA152@users.noreply.github.com>
2020-08-03 11:56:54 +0800

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:25:05 2020
+
+@author: AE401
+""" 
+number = input("請輸入半徑?")
+number = float(number)
+a = 2*3.14*number
+b = 3.14*number*number
+print("周長",a)
+print("面積",b)
